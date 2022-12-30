faeda8e982875424969ec9a23501de7ac10fe0cc
NhiPham <nhi.phamt2002@gmail.com>
2022-12-30 14:08:18 +0700

    add search filter

---
+++ app/apis/searchAPI_blueprint.py
@@ -0,0 +1,37 @@
+from sanic import Sanic
+from sanic.response import json
+from sanic_openapi import openapi2_blueprint
+from sanic_openapi import swagger_blueprint
+from sanic_openapi.openapi2 import doc
+
+from app.databases.filter import SearchFilter
+
+app = Sanic(name="SearchAPI")
+app.blueprint(openapi2_blueprint)
+# app.blueprint(swagger_blueprint)
+
+esearch = SearchFilter()
+
+@app.route("/search")
+@doc.tag("Elastic Search Test")
+@doc.summary("Search data by type and string")
+@doc.consumes(doc.String(name="index_type", description="'projects or 'smart_contracts'"), location="query",
+              required=True)
+@doc.consumes(doc.String(name="search_str"), location="query", required=True)
+@doc.consumes(doc.Integer(name="limit"), location="query")
+@doc.response(400, {"message": str}, description="Bad Request")
+async def search(request):
+    args = request.args
+    search_str = args.get("search_str")
+    index_type = args.get("index_type")
+    limit = int(args.get('limit'))
+    if limit <= 0 or limit >= 10000:
+        limit = 10  # default value
+    if index_type == "projects":
+        result = esearch.projects_search_v1(search_str=search_str, limit=limit)
+    elif index_type == "smart_contracts":
+        result = esearch.contract_search_v1(search_str=search_str, limit=limit)
+    return result
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
---
+++ app/databases/filter.py
@@ -0,0 +1,125 @@
+from elasticsearch import Elasticsearch
+from sanic import json
+
+
+class SearchFilter:
+    def __init__(self, connection_url="http://localhost:9200"):
+        self.connection_url = connection_url
+        self.es = Elasticsearch(self.connection_url)
+
+    def projects_search_v1(self, search_str, limit=10):
+        if search_str is None:
+            return None
+        index = "projects"
+        query = {
+            "bool": {
+                "must": [
+                    {"fuzzy": {
+                        "description": {
+                            "value": "{}".format(search_str),
+                            "fuzziness": 2,
+                            "max_expansions": 50,
+                            "prefix_length": 0,
+                            "transpositions": "false",
+                            "rewrite": "constant_score"
+                        }
+                    }}
+                ],
+                "should": [
+                    {"wildcard": {
+                        "name": {
+                            "value": "{}*".format(search_str),
+                            "boost": 1.5,
+                            "case_insensitive": "true",
+                            "rewrite": "constant_score"
+                        }
+                    }},
+                    {"match_phrase_prefix": {
+                        "name": "{}".format(search_str)
+                    }}
+                ],
+                "filter": [],
+                "must_not": []
+            }
+        }
+        resp = self.es.search(index=index, query=query, size=limit)
+        hits_ = []
+        total_hits = resp["hits"]["total"]["value"]
+        for i in resp["hits"]["hits"]:
+            hit_ = {
+                "_id": i["_id"],
+                "_score": i["_score"],
+                "name": i["_source"]["name"],
+                "_description": i["_source"]["description"]
+            }
+            hits_.append(hit_)
+        return json({"total_hits": total_hits,
+                     "hits": hits_})
+
+    def contract_search_v1(self, search_str, limit = 10):
+        if search_str is None:
+            return None
+        index = "smart_contracts"
+        query = {
+            "bool": {
+                "must": [
+                    {"fuzzy": {
+                        "name": {
+                            "value": "{}".format(search_str),
+                            "fuzziness": "AUTO",
+                            "max_expansions": 50,
+                            "prefix_length": 0,
+                            "transpositions": "false",
+                            "rewrite": "constant_score"
+                        }
+                    }}
+                    # {
+                    #     "wildcard": {
+                    #         "name": {
+                    #             "value": "*{}*".format(search_str),
+                    #             "boost": 1.0,
+                    #             "case_insensitive": "true",
+                    #             "rewrite": "constant_score"
+                    #         }
+                    #     }
+                    # }
+                ],
+                "should": [
+                    {"wildcard": {
+                        "name": {
+                            "value": "*{}*".format(search_str),
+                            "boost": 1.5,
+                            "case_insensitive": "true",
+                            "rewrite": "constant_score"
+                        }
+                    }},
+                    {"match_phrase_prefix": {
+                        "name": "{}".format(search_str)
+                    }}
+                    # {"fuzzy": {
+                    #     "name": {
+                    #         "value": "{}".format(search_str),
+                    #         "fuzziness": "AUTO",
+                    #         "max_expansions": 20,
+                    #         "prefix_length": 0,
+                    #         "transpositions": "false",
+                    #         "rewrite": "constant_score"
+                    #     }
+                    # }}
+                ],
+                "filter": [],
+                "must_not": []
+            }
+        }
+        resp = self.es.search(index=index, query=query, size=limit)
+        hits_ = []
+        total_hits = resp["hits"]["total"]["value"]
+        for i in resp["hits"]["hits"]:
+            hit_ = {
+                "_id": i["_id"],
+                "_score": i["_score"],
+                "name": i["_source"]["name"],
+            }
+            hits_.append(hit_)
+        return json({"total_hits": total_hits,
+                     "hits": hits_})
